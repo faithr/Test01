0375fefe50eb6fb51c574985d434079ab8ce577e
Faith Rothenberg <frothenberg@optonline.net>
2016-03-06 17:52:13 -0500

    Initial before deleting css flexbox commands

---
+++ public/js/controllers/TestCtrl.js
@@ -0,0 +1,19 @@
+/**
+ * Created by froth on 3/6/2016.
+ */
+angular.module("MyApp")
+    .controller('TestCtrl', testCtrl);
+
+function testCtrl() {
+    var vm = this;
+
+    vm.title = "Faith Test App";
+    vm.subtitle = "You gotta have Faith";
+    vm.text = {
+"title": "faith",
+        "address": "28 crescent road",
+        "state": "nj"
+    }
+
+
+}
---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by froth on 3/6/2016.
+ */
+
+angular
+    .module('MyApp',['ngMaterial', 'ngMessages', 'ui.router']);
